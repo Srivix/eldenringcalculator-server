e766ef6ba5f0f66bb62b886b1f9a815b334cb72a
Ivan Ciudad Espinar <ivan.ciudad-espinar@capgemini.com>
2022-03-03 10:09:12 +0100

    Validaciones Stats y Requirements

--- eldenringcalculator/src/main/java/com/eldenringcalculator/build/BuildServiceImpl.java
+++ eldenringcalculator/src/main/java/com/eldenringcalculator/build/BuildServiceImpl.java
@@ -29,8 +29,6 @@ public class BuildServiceImpl implements BuildService{
 	public void save(Long id, BuildDto dto) {
 		BuildEntity build = null;
 		
-		Boolean validated = true;
-		
 		if(id == null)
 			build = new BuildEntity();
 		else
@@ -38,22 +36,39 @@ public class BuildServiceImpl implements BuildService{
 		
 		BeanUtils.copyProperties(dto, build, "id", "weapon1", "weapon2");
 		
+		if(validateRequirements(dto, build) && validateStats(build))
+			this.buildRepository.save(build);
+	}
+	
+	public Boolean validateRequirements(BuildDto dto, BuildEntity build) {
+		
 		if(dto.getWeapon1()!=null)
 			if(build.getDexterity()>=dto.getWeapon1().getDexreq() && build.getStrength()>=dto.getWeapon1().getStrengreq() && build.getIntelect()>=dto.getWeapon1().getIntreq())
 				build.setWeapon1(weaponService.get(dto.getWeapon1().getId()));
-			else
-				validated = false;
+			else {
+				System.out.println("Te falta estadistica en el arma 1 para poder equiparla");
+				return false;
+			}
 
 		if(dto.getWeapon2()!=null)
 			if(build.getDexterity()>=dto.getWeapon2().getDexreq() && build.getStrength()>=dto.getWeapon2().getStrengreq() && build.getIntelect()>=dto.getWeapon2().getIntreq())
 				build.setWeapon2(weaponService.get(dto.getWeapon2().getId()));
-			else
-				validated = false;
+			else {
+				System.out.println("Te falta estadistica en el arma 2 para poder equiparla");
+				return false;
+			}
 		
-		if(validated)
-			this.buildRepository.save(build);
-		else
-			System.out.println("Atributo más bajo que el del arma.");
+		return true;
+	}
+	
+	public Boolean validateStats(BuildEntity build) {
+		
+		if(build.getDexterity()+build.getIntelect()+build.getStrength()==build.getLevel())		
+			return true;
+		else {
+			System.out.println("La suma de atributos no suma con el nivel.");
+			return false;
+		}
 	}
 
 }
